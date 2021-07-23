6e3e06e1d1b430702bdad3c543c95743091f812d
3O1 <jamesbrandonedwards301@gmail.com>
2021-07-23 12:53:53 -0400

    Added signup Mutation and basic UI to signup.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,31 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import React from "react";
 import Users from "./components/Users";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
+import { setContext } from "apollo-link-context";
+import Signup from "./pages/Signup";
 
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: link as any,
   cache: new InMemoryCache(),
 });
 
@@ -16,6 +36,9 @@ function App() {
         <Switch>
           <Route path="/landing">
             <Landing />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
           </Route>
           <Route exact path="/">
             <Users />
